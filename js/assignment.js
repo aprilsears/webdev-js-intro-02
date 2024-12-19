df8e02911f1db75fced2217f192874918b485314
April Sears <aprilnsears@gmail.com>
2024-12-19 10:09:18 -0500

    updated JS assignment, readme, and index

--- js/assignment.js
+++ js/assignment.js
@@ -2,34 +2,42 @@
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
+
+const resolutionElement = document.getElementById("resolution");
+const willMeetResolutionElement = document.getElementById("will-meet-resolution");
 const yearElement = document.getElementById("year");
-const resolutionElement = document.getElementById("resolution")
-const willMeetResolutionElement = document.getElementById("will-meet-resolution")
-const submissionBtn = document.getElementById("submission-btn")
+const submissionBtn = document.getElementById("submission-btn");
 
 // Declare your variables here.
-
+const resolution = "I don't set resolutions for the New Year"; 
+const currentYear = 2025;
+let willMeetResolution = false;
 function updateYear() {
     // Update this function
+    yearElement.textContent = currentYear;
 
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution;
 
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+    willMeetResolutionElement.textContent = willMeetResolution;
 }
 
 function render() {
     // Update this function
+    updateResolution();
+    updateYear();
+    updateWillMeetResolution();
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+    render();
 })
--- js/index.js
+++ js/index.js
@@ -2,3 +2,18 @@
 // that are not declared
 "use strict"
 
+const myName = "April";
+console.log(myName)     // Output: "April"
+console.log(typeof myName)   // Output: "string"
+
+const one = 1;
+console.log(one)                // Output: 1
+console.log(typeof one);        // Output: "number"
+
+let isMentor = false;            
+console.log(isMentor)           // Output: false
+console.log(typeof isMentor)    // Output: "boolean"
+
+const age = 39;
+console.log(age);           // Output:39
+console.log(typeof age);    // Output: "number"
